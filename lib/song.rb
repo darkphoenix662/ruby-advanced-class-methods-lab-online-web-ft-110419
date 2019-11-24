36c96f94662d66229d8ace491c720961ffec6a91
Sean Good <tree662@protonmail.com>
2019-11-24 00:13:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,9 +5,50 @@ class Song
   def self.all
     @@all
   end
-
+  
   def save
-    self.class.all << self
+    self.class.all << self 
   end
-
+  
+  def self.create 
+    song = Song.new 
+    song.save
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new            #passingCuric
+    song.name = name
+    song 
+  end
+  
+  def self.create_by_name(name)
+    song = self.create            #passingCuric
+    song.name = name 
+    song 
+  end
+  
+  def self.find_by_name(name)
+    self.all.find{|song| song.name == name}    #passingRubyDoc
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)  #passingCoderW
+  end
+  
+  def self.alphabetical
+    self.all.sort_by{|n| n.name}     #passingRubGuidVid
+  end
+  
+  def self.new_from_filename(file)
+    
+  end
+  
+  def self.create_from_filename(file)
+    
+  end
+  
+  def self.destroy_all
+    @@all = []
+  end
 end
