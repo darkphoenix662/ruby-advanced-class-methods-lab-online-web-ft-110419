b0447b1464c0fcd0048affe0333c62fb673a791f
Sean Good <tree662@protonmail.com>
2019-11-24 02:55:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,7 +43,7 @@ class Song
   def self.new_from_filename(file)
     rows = file.split("-")
       artist_name = rows[0]
-      song_title = rows[1].sub(".mp3", "")
+      song_title = rows[1].gsub(".mp3", "")
       song = self.create 
       song.name = name
       song.artist_name = artist_name
